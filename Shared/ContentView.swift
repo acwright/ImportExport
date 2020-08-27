bf49a45b61fccec9e45d114af13cd7a6124f997a
Aaron Wright <aaron.c.wright@icloud.com>
2020-08-27 12:13:41 -0500

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Aaron Wright on 8/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/ImportExportApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImportExportApp.swift
+//  Shared
+//
+//  Created by Aaron Wright on 8/27/20.
+//
+
+import SwiftUI
+
+@main
+struct ImportExportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
